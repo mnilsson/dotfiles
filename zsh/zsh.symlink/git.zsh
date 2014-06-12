f5648dec0a374c7ca84489464966d5cb6ee37af1
Markus Nilsson <markus@mnilsson.se>
2014-06-12 11:01:42 +0200

    Some updates to zsh

--- zsh/zsh.symlink/git.zsh
+++ zsh/zsh.symlink/git.zsh
@@ -1,12 +1,12 @@
 function git_prompt_info() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  echo "(${ref#refs/heads/} $(parse_git_dirty))"
+  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
 }
 
 function parse_git_dirty() {
   if [[ -n $(git status -s --ignore-submodules=dirty 2> /dev/null) ]]; then
-    echo "x"
+    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
   else
-    echo "o"
+    echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
   fi
 }
