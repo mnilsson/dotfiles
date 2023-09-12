39d7d0a929e7c2afa036646237e7c47e51c464ef
Markus Nilsson <markus@mnilsson.se>
2023-09-12 15:57:11 +0200

    use lazy

--- nvim/lua/mnilsson/plugins.lua
+++ nvim/lua/mnilsson/plugins.lua
@@ -1,102 +1,124 @@
-
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then 
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath
+  })
 end
 
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
+vim.opt.rtp:prepend(lazypath)
 
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  return
-end
 
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float {border = "rounded" }
-    end,
+require("lazy").setup({
+  "wbthomason/packer.nvim",
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
+
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "hrsh7th/cmp-cmdline",
+  "hrsh7th/nvim-cmp",
+
+  "hrsh7th/vim-vsnip",
+  "hrsh7th/cmp-vsnip",
+
+  "saadparwaiz1/cmp_luasnip", -- Snippets source for nvim-cmp
+  "L3MON4D3/LuaSnip", -- Snippets plugin
+
+
+  "folke/tokyonight.nvim",
+
+  {
+    "kyazdani42/nvim-tree.lua",
+    lazy = false,
+    dependencies = {
+      "kyazdani42/nvim-web-devicons",
+    },
+    config = function () 
+      require("nvim-tree").setup {
+        
+        sync_root_with_cwd = true,
+        respect_buf_cwd = true,
+        update_focused_file = {
+          enable = true,
+          update_root = true,
+        }
+      }
+    end
   },
-}
 
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+  "nvim-lualine/lualine.nvim",
 
-  use {
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'neovim/nvim-lspconfig',
+  "akinsho/bufferline.nvim",
 
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-cmdline',
-    'hrsh7th/nvim-cmp',
 
-    'hrsh7th/vim-vsnip',
-    'hrsh7th/cmp-vsnip',
+  'numtostr/comment.nvim',
 
-    'saadparwaiz1/cmp_luasnip', -- Snippets source for nvim-cmp
-    'L3MON4D3/LuaSnip' -- Snippets plugin
-  }
+  "ahmedkhalf/project.nvim",
 
-  -- themes and schemes
-  use { "folke/tokyonight.nvim" }
+  'simrat39/rust-tools.nvim',
 
-  use { 'kyazdani42/nvim-web-devicons', "kyazdani42/nvim-tree.lua" }
-
-  use { 'nvim-lualine/lualine.nvim' }
-
-  use { 'akinsho/bufferline.nvim' }
-  use {
+  "mfussenegger/nvim-dap",
+  "rcarriga/nvim-dap-ui",
+  "ravenxrz/dapinstall.nvim",
+  {
     'nvim-treesitter/nvim-treesitter',
     config = function()
     end,
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-  }
-
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
-  use {
+    build = function()
+      require('nvim-treesitter.install').update({ with_sync = true }) 
+    end,
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.0',
+    dependencies = { {'nvim-lua/plenary.nvim'} }
+  },
+  {
     "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = {"nvim-lua/plenary.nvim"},
     config = function()
       require('neorg').setup {
         load = {
           ["core.defaults"] = {},
-          ["core.norg.concealer"] = {},
-          ["core.norg.dirman"] = {
+          ["core.concealer"] = {},
+          ["core.esupports.metagen"] = { config = { type = "auto", update_date = true } },
+          ["core.qol.toc"] = {},
+          ["core.qol.todo_items"] = {},
+          ["core.looking-glass"] = {},
+          ["core.presenter"] = { config = { zen_mode = "zen-mode" } },
+          ["core.journal"] = {
+            config = {
+              strategy = "flat",
+              workspace = "Notes",
+            },
+          },
+          ["core.dirman"] = {
             config = {
               workspaces = {
-                work = "~/notes/work",
-                home = "~/notes/home",
+                work = "~/projects/mnilsson/notes/work",
+                home = "~/projects/mnilsson/notes/home",
               }
             }
-          }
+          },
+          ["core.keybinds"] = {
+            config = {
+              default_keybinds = true,
+              neorg_leader = "<Leader><Leader>",
+            },
+          },
         }
       }
     end,
-    requires = "nvim-lua/plenary.nvim"
   }
+})
 
-  use { 'numToStr/Comment.nvim'}
 
-  use { "ahmedkhalf/project.nvim" }
 
-  use { 'simrat39/rust-tools.nvim' }
-
-  use {"mfussenegger/nvim-dap","rcarriga/nvim-dap-ui","ravenxrz/DAPInstall.nvim"}
-
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
